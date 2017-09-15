c8183ccf5fbc45aa7f5fa4e2c1a97656eb2e990a
devnev <mark.nevill@gmail.com>
2017-09-15 16:54:11 +0100

    Extract tag-based metric labels for both in and outbound requests. (#28)

--- metrics/prometheus/meta.go
+++ metrics/prometheus/meta.go
@@ -15,26 +15,29 @@ type meta struct {
 
 func reqMeta(req *http.Request, opts *options, inbound bool) *meta {
 	m := &meta{name: opts.name}
-	if m.name == "" && inbound {
-		v := http_ctxtags.ExtractInbound(req).Values()[http_ctxtags.TagForCallService]
-		m.name, _ = v.(string)
-	}
-	if m.name == "" && !inbound {
-		v := http_ctxtags.ExtractOutbound(req).Values()[http_ctxtags.TagForCallService]
-		m.name, _ = v.(string)
-	}
+
+	var tags map[string]interface{}
 	if inbound {
-		v := http_ctxtags.ExtractInbound(req).Values()[http_ctxtags.TagForHandlerName]
-		hname, _ := v.(string)
-		if hname != "" {
-			v := http_ctxtags.ExtractInbound(req).Values()[http_ctxtags.TagForHandlerGroup]
-			hgroup, _ := v.(string)
-			if hgroup == "" {
-				hgroup = "unknown"
-			}
-			m.handler = hgroup + "." + hname
-		}
+		tags = http_ctxtags.ExtractInbound(req).Values()
+	} else {
+		tags = http_ctxtags.ExtractOutbound(req).Values()
 	}
+	var v interface{}
+	if m.name == "" {
+		v, _ = tags[http_ctxtags.TagForCallService]
+		m.name, _ = v.(string)
+	}
+	v, _ = tags[http_ctxtags.TagForHandlerName]
+	hname, _ := v.(string)
+	if hname != "" {
+		v, _ = tags[http_ctxtags.TagForHandlerGroup]
+		hgroup, _ := v.(string)
+		if hgroup == "" {
+			hgroup = "unknown"
+		}
+		m.handler = hgroup + "." + hname
+	}
+
 	if opts.hosts {
 		m.host = req.URL.Host
 		if m.host == "" {
