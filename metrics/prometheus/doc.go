cf0ba67a0bd20b1ca9df871735cf61d8c5e18bb2
Bartek Plotka <bwplotka@gmail.com>
2017-09-18 15:05:36 +0100

    Added DOCs for Prometheus.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

---
+++ metrics/prometheus/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2017 Mark Nevill. All Rights Reserved.
+// See LICENSE for licensing terms.
+
+/*
+`http_prometheus` provides Prometheus-based client and server side reporting of HTTP request and response metrics.
+The user may choose what level of detail is included using options to these reporters.
+*/
+package http_prometheus
