ed9ec5e74f4224cc61c7b0dcb7f563a463138c1f
Mark Nevill <mark.nevill@gmail.com>
2017-09-18 14:17:56 +0100

    Include handler in client metrics, add test.

--- metrics/prometheus/client.go
+++ metrics/prometheus/client.go
@@ -19,14 +19,14 @@ var (
 			Name: "http_tripper_started_requests_total",
 			Help: "Count of started requests.",
 		},
-		[]string{"name", "host", "path", "method"},
+		[]string{"name", "handler", "host", "path", "method"},
 	)
 	clientCompleted = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "http_tripper_completed_requests_total",
 			Help: "Count of completed requests.",
 		},
-		[]string{"name", "host", "path", "method", "status"},
+		[]string{"name", "handler", "host", "path", "method", "status"},
 	)
 	clientLatency = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
@@ -34,7 +34,7 @@ var (
 			Help:    "Latency of completed requests.",
 			Buckets: []float64{.01, .03, .1, .3, 1, 3, 10, 30, 100, 300},
 		},
-		[]string{"name", "host", "path", "method", "status"},
+		[]string{"name", "handler", "host", "path", "method", "status"},
 	)
 	clientRequestSize = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
@@ -42,7 +42,7 @@ var (
 			Help:    "Size of sent requests.",
 			Buckets: prometheus.ExponentialBuckets(32, 32, 6),
 		},
-		[]string{"name", "host", "path", "method", "status"},
+		[]string{"name", "handler", "host", "path", "method", "status"},
 	)
 	clientResponseSize = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
@@ -50,7 +50,7 @@ var (
 			Help:    "Size of received responses.",
 			Buckets: prometheus.ExponentialBuckets(32, 32, 6),
 		},
-		[]string{"name", "host", "path", "method", "status"},
+		[]string{"name", "handler", "host", "path", "method", "status"},
 	)
 
 	clientInit     sync.Once
@@ -95,25 +95,25 @@ type clientTracker struct {
 }
 
 func (t *clientTracker) RequestStarted() {
-	clientStarted.WithLabelValues(t.name, t.host, t.path, t.method).Inc()
+	clientStarted.WithLabelValues(t.name, t.handler, t.host, t.path, t.method).Inc()
 }
 
 func (t *clientTracker) RequestRead(duration time.Duration, size int) {
 	if t.opts.sizes {
-		clientRequestSize.WithLabelValues(t.name, t.host, t.path, t.method).Observe(float64(size))
+		clientRequestSize.WithLabelValues(t.name, t.handler, t.host, t.path, t.method).Observe(float64(size))
 	}
 }
 
 func (t *clientTracker) ResponseStarted(duration time.Duration, code int, header http.Header) {
 	status := strconv.Itoa(code)
-	clientCompleted.WithLabelValues(t.name, t.host, t.path, t.method, status).Inc()
+	clientCompleted.WithLabelValues(t.name, t.handler, t.host, t.path, t.method, status).Inc()
 	if t.opts.latency {
-		clientLatency.WithLabelValues(t.name, t.host, t.path, t.method, status).Observe(duration.Seconds())
+		clientLatency.WithLabelValues(t.name, t.handler, t.host, t.path, t.method, status).Observe(duration.Seconds())
 	}
 }
 
 func (t *clientTracker) ResponseDone(duration time.Duration, code int, size int) {
 	if t.opts.sizes {
-		clientResponseSize.WithLabelValues(t.name, t.host, t.path, t.method, strconv.Itoa(code)).Observe(float64(size))
+		clientResponseSize.WithLabelValues(t.name, t.handler, t.host, t.path, t.method, strconv.Itoa(code)).Observe(float64(size))
 	}
 }
